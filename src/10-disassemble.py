7912cb8e92b682b6eaf869dbdb491dcc324992b8
Param Raghavan <paramraghavan@gmail.com>
2021-06-29 07:01:01 -0400

    .

--- src/10-disassemble.py
+++ src/10-disassemble.py
@@ -1,7 +1,7 @@
 '''
 dis,dis --> displays the python byte code. But this is not the code which runs
-inside the CPU - that is the assembly code (Compile python to C, using Cython,
-then use a C compiler of your choice to get it down to assembly.)
+inside the CPU - that is the assembly code. To get to assembly code - Compile python to C,
+using Cython, then use a C compiler of your choice to get it down to assembly.
 '''
 
 import dis
