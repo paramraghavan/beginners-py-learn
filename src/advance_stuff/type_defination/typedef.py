891a29b158df88ea88f70b54af90b19d97631f1b
Param Raghavan <paramraghavan@gmail.com>
2022-02-11 10:45:11 -0500

    python type defination

--- src/advance_stuff/type_defination/typedef.py
+++ src/advance_stuff/type_defination/typedef.py
@@ -1,6 +1,7 @@
 '''
 
-Note The Python runtime does not enforce function and variable type annotations. They can be
+Note::
+ The Python runtime does not enforce function and variable type annotations. They can be
 used by third party tools such as type checkers, IDEs, linters, etc.
 
 Important things to note about type hints:
