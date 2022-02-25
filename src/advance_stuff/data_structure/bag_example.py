896ddf7049ff614080f68fa8da436d1da1825de4
Param Raghavan <paramraghavan@gmail.com>
2022-02-25 06:23:13 -0500

    updated

--- src/advance_stuff/data_structure/bag_example.py
+++ src/advance_stuff/data_structure/bag_example.py
@@ -6,7 +6,8 @@ bag_item = Bag();
 bag_item.add('item1')
 bag_item.add('item2')
 
-print(bag_item)
+print(f'repr {bag_item}')
+print(f'type {type(bag_item)}')
 
 for item in bag_item:
     print(item)
