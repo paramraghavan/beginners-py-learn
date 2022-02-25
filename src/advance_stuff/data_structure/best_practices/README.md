f65706c32916e3d3b53cb26e87ebbebb3ea703ce
Param Raghavan <paramraghavan@gmail.com>
2022-02-25 07:13:30 -0500

    python and memory management

---
+++ src/advance_stuff/data_structure/best_practices/README.md
@@ -0,0 +1,12 @@
+# Python Memory Management
+
+- In Python everything is an object. This means that Dynamic Memory Allocation underlies Python Memory Management.
+When objects are no longer needed, the Python Memory Manager will automatically reclaim memory from them.
+
+-
+
+
+# References:
+- https://realpython.com/python-memory-management/
+- https://docs.python.org/3/c-api/memory.html
+- https://towardsdatascience.com/memory-management-in-python-6bea0c8aecc9
---
+++ src/advance_stuff/data_structure/best_practices/best_practice_one.py
@@ -0,0 +1,83 @@
+'''
+Instead of adding line1, line2 to mymsg individually, use list and join.
+'''
+
+# Don't
+mymsg='line1\n'
+mymsg+='line2\n'
+
+
+# Better choice:
+mymsg=['line1','line2']
+'\n'.join(mymsg)
+
+
+'''
+Avoid using the + operator for strings
+Don’t use the + operator for concatenation if you can avoid it. Because strings are immutable, every time you add an element to a string, 
+Python creates a new string and a new address. This means that new memory needs to be allocated each time the string is altered
+'''
+
+# Don't
+msg='hello'+mymsg+'world'
+
+
+# Better
+msg='hello %s world' % mymsg
+msg= f'hello {mymsg} world'
+
+
+'''
+Use Generators
+Generators allow you to create a function that returns one item at a time rather than all the items at once.
+This means that if you have a large dataset, you don’t have to wait for the entire dataset to be accessible.
+'''
+def __iter__(self):
+    return self._generator()
+
+
+def _generator(self):
+    for itm in self.items():
+        yield itm
+
+# Put evaluations outside the loop
+
+'''
+If you are iterating through data, you can use the cached version of the regex.
+'''
+import re
+match_regex=re.compile("foo|bar")
+
+# some string
+big_it = None
+
+for i in big_it:
+    m = match_regex.search(i)
+    pass
+
+'''
+Assign a function to a local variable, especially when invoking inside a loop.
+Python accesses local variables much more efficiently than global variables. 
+Assign functions to local variables then use them.
+
+myLocalFunc=myObj.func
+for i in range(n):
+    myLocalFunc(i)
+
+'''
+
+
+'''
+Use built-in functions and libraries
+Use built-in functions and libraries whenever you can. Built-in functions are often implemented using the best memory usage practices.
+'''
+
+# Dont' do this
+oldList =  ['abcSSS', 'ererWWWWW']
+mylist=[]
+for myword in oldList:
+      mylist.append(myword.lower())
+
+
+# Better choice
+mylist = map(str.lower(), oldList)
