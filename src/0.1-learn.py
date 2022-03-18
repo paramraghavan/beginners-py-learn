fa64404b287a071e051d2e4105c2821b1c47466a
Param Raghavan <paramraghavan@gmail.com>
2022-03-17 22:50:42 -0400

    double division

---
+++ src/0.1-learn.py
@@ -0,0 +1,9 @@
+'''
+The Double Division operator in Python returns the floor value for both integer
+and floating-point arguments after division.
+'''
+length = 4
+middle = (length+1)/2 #2.5
+print(f'middle/ : {middle}')
+middle = (length+1)//2 #2
+print(f'middle// : {middle}')
