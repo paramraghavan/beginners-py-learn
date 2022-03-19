85b97c674ec16efc325bbefd05498cfb09118c19
Param Raghavan <paramraghavan@gmail.com>
2022-03-19 08:02:52 -0400

    exercise 6 - generate range

--- src/exercises/exercise6/exercise6_solution.py
+++ src/exercises/exercise6/exercise6_solution.py
@@ -6,26 +6,43 @@ gen_range(100, 140, 10)  would produce output:
 '''
 
 
-def gen_range(start, stop, step):
-    for i in range(start,stop+1,step):
-         yield (i,i+step)
-
-
 def gen_range_values(start, stop, step):
+    range_values = []
+    for item in range(start, stop, step):
+        range_values.append((item,item+step))
+
+    print(f'range_values: {range_values}')
+    return range_values
+
+'''
+Another solution to create range list using python 'generator'
+'''
+def gen_range_values_alt(start, stop, step):
     range_values = []
     for range in gen_range(start, stop, step):
         if range[0] == stop:
             break;
         range_values.append(range)
+
     print(f'range_values: {range_values}')
+    return range_values
+
+'''
+Creating generator function using yield
+'''
+def gen_range(start, stop, step):
+    for i in range(start,stop+1,step):
+         yield (i,i+step)
+
 
 
 if __name__ == '__main__':
     start = 100
     stop = 190
     step = 10
-    gen_range_values(start, stop,step)
-
+    range_values = gen_range_values(start, stop,step)
+    range_values_alt = gen_range_values_alt(start, stop, step)
+    assert range_values == range_values_alt, 'Error one or both of the range generators are incorrect.'
 
 
 
