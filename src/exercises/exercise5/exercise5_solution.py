4277bb499bec5bee152672f9bdaf86eeb22def10
Param Raghavan <paramraghavan@gmail.com>
2022-03-19 12:08:42 -0400

    exercise5 optimized for BigOn

--- src/exercises/exercise5/exercise5_solution.py
+++ src/exercises/exercise5/exercise5_solution.py
@@ -81,14 +81,64 @@ def map_range_values_alt(range, values):
     return map_of_range_values
 
 
+'''
+BigO is range items count X values items count --> mXn, n^2
+Can we reduce the BigO from the order of n^2 ?
+Assuming:
+ 1. Range values start/stop are exclusive to each tuple and are in sorted order
+ 2. The values in list_values are also sorted
+ref: https://www.tutorialspoint.com/python_data_structure/python_divide_and_conquer.htm
+'''
+
+def check_in_range(num, range_values):
+    match = None
+    for range in range_values:
+        if range[0] <= num <= range[1]:
+            match = range
+            break
+
+    return match
+
+
+
+def map_range_values_both_sorted(range, values):
+    map_of_range_values = {}
+    global loop_counter
+    loop_counter = 0
+    # values loop
+    range_length = len(range)
+    for num in values:
+        loop_counter += 1
+        range_matched = check_in_range(num, range)
+        if range_matched is not None and num >= range_matched[1]:
+            range.remove(range_matched)
+            if len(range) == 0:
+                break;
+        if range_matched == None:
+            print(f'No match found for num: {num}')
+            continue # no match found for num
+        if  map_of_range_values.get(range_matched, None) == None:
+            map_of_range_values[range_matched] = [num]
+        else:
+            map_of_range_values[range_matched].append(num)
+
+    return map_of_range_values
+
+
+
+
 
 if __name__ == '__main__':
     list_range = [(0,9), (10, 20), (21, 30), (40, 60), (70, 90), (91, 100)]
     list_values = [10, 13, 23, 65, 71, 32, 45, 67, 89, 90, 27, 99, 101]
 
     ''' 
-    You will notice that the number for iterations have reduced with function map_range_values_alt
-    map_of_range_values: {(10, 20): [10, 13], (21, 30): [23, 27], (40, 60): [45], (70, 90): [89, 89, 90], (91, 100): [99]}, loop_counter: 70        
+    You will notice that the number for iterations have reduced:
+    Original --> map_of_range_values: {(0, 9): [], (10, 20): [10, 13], (21, 30): [23, 27], (40, 60): [45], (70, 90): [71, 89, 90], (91, 100): [99]}, loop_counter: 84
+    Assuming range sorted --> map_of_range_values_sorted: {(10, 20): [10, 13], (21, 30): [23, 27], (70, 90): [71, 89, 90], (40, 60): [45], (91, 100): [99]}, loop_counter: 45
+    Assuming range and values both sorted --> map_range_values_both_sorted: {(10, 20): [10, 13], (21, 30): [23, 27], (40, 60): [45], (70, 90): [71, 89, 90], (91, 100): [99]}, loop_counter: 13       
     '''
     print(f'map_of_range_values: {map_range_values(list_range, list_values)}, loop_counter: {loop_counter}')
-    print(f'map_of_range_values: {map_range_values_alt(list_range, list_values)}, loop_counter: {loop_counter}')
+    print(f'map_of_range_values_sorted: {map_range_values_alt(list_range, list_values)}, loop_counter: {loop_counter}')
+    list_values_in_sort_order = [10, 13, 23, 27, 32, 45, 65, 71, 67, 89, 90, 99, 101]
+    print(f'map_range_values_both_sorted: {map_range_values_both_sorted(list_range, list_values_in_sort_order)}, loop_counter: {loop_counter}')
