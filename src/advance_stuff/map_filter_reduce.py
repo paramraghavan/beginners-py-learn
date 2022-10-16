72efeab7fa9210d7269bf240ca2d5245c8bf7e6d
Param Raghavan <52529498+paramraghavan@users.noreply.github.com>
2022-10-16 10:14:15 -0400

    Update map_filter_reduce.py

--- src/advance_stuff/map_filter_reduce.py
+++ src/advance_stuff/map_filter_reduce.py
@@ -19,4 +19,40 @@ def student_pass_grade_check(mark):
     return mark >= 70
 pass_marks = filter(student_pass_grade_check, marks)
 print(list(pass_marks))    
-[70, 99]
+#[70, 99]
+
+
+```
+reduce is not builtin  like map and filter, need to import module functools
+from functools import reduce
+
+reduce(...)
+    reduce(function, sequence[, initial]) -> value
+    
+    Apply a function of two arguments cumulatively to the items of a sequence,
+    from left to right, so as to reduce the sequence to a single value.
+    For example, reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]) calculates
+    ((((1+2)+3)+4)+5).  If initial is present, it is placed before the items
+    of the sequence in the calculation, and serves as a default when the
+    sequence is empty.
+'''
+
+from functools import reduce
+numbers = [7, 8, 3, 11, 15, 20]
+def sum2(first, second):
+    return first + second
+val = reduce(sum2, numbers)
+print(val)
+# 64
+
+#using intial value with reduce
+from functools import reduce
+numbers = [7, 8, 3, 11, 15, 20]
+def sum2(first, second):
+    return first + second
+val = reduce(sum2, numbers, 100)
+print(val)
+# 164
+
+
+    
