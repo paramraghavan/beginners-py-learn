9a56977fd3cd057a95feaf8017e0b63e3216ba96
paramraghavan <paramraghavan@gmail.com>
2024-04-05 12:44:52 -0400

    folder name fixed

--- src/advance_stuff/getattr_locals/usecase1_getattr.py
+++ src/advance_stuff/getattr_locals/usecase1_getattr.py
@@ -3,6 +3,9 @@ class Person:
         self.name = name
         self.age = age
 
+    def printName(self):
+        print(self.name)
+
 if __name__ == '__main__':
     person = Person("Alice", 30)
     attribute_name = "age"
@@ -10,4 +13,6 @@ if __name__ == '__main__':
     print(age)  # Output: 30
     attribute_name = "address"
     address = getattr(person, attribute_name, f"Attribute {attribute_name} not found")
+    attribute_name = "printName"
+    getattr(person, attribute_name, f"Attribute {attribute_name} not found")()
     print(address)  # Attribute address not found
